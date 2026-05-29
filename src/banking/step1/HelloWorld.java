1a860edbbd30cd431fa6c6796e8fe7abc1592885
nakjasabal <nakjasabal@naver.com>
2026-05-29 10:51:31 +0900

    순번적용함

--- src/banking/step1/HelloWorld.java
+++ src/banking/step1/HelloWorld.java
@@ -1,11 +1,11 @@
 package banking.step1;
 
 public class HelloWorld {
-	//github연동 
-	//연동완료 후 커밋&푸시 
-	//추가작업
-	//학원에서 pull없이 작업함
-	//github clone 작업 성공
+	//1github연동 
+	//2연동완료 후 커밋&푸시 
+	//3추가작업
+	//4학원에서 pull없이 작업함
+	//5github clone 작업 성공
 	public static void main(String[] args) {		
 		System.out.print("깃헙");
 
