33c207973b6c9fef278ce157ecdd640200229761
nakjasabal <nakjasabal@naver.com>
2026-05-29 10:15:37 +0900

    집에서작업1

--- src/banking/step1/HelloWorld.java
+++ src/banking/step1/HelloWorld.java
@@ -4,9 +4,9 @@ public class HelloWorld {
 	//github연동 
 	//연동완료 후 커밋&푸시 
 	//추가작업
-	public static void main(String[] args) {
-		
-
+	//github clone 작업 성공
+	public static void main(String[] args) {		
+		System.out.print("깃헙");
 	}
 
 }
